941e1a3d1a07686a1fcfef8fa333e405eb37ae75
RyosukeMatsushima <m.ryosuke@me.com>
2023-05-15 23:14:18 +0900

    add transition matrix calculation

---
+++ modules/states_space/transition_matrix.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+from scipy.sparse import lil_matrix
+from scipy.sparse.linalg import cg
+
+from modules.states_space.states_space import StatesSpace
+
+# TODO: remove
+from modules.tools.fig_2d import *
+
+
+def get_transition_matrix(state_space, dynamic_function, inputs_set, time_step):
+    matrix_set = [
+        lil_matrix((state_space.element_count, state_space.element_count))
+        for _ in inputs_set
+    ]
+
+    for element_number in range(state_space.element_count):
+        states = state_space.get_states_from(element_number)
+
+        neighbors = state_space.get_neighbors_element_number(element_number)
+
+        for inputs, matrix in zip(inputs_set, matrix_set):
+            transition_vector = dynamic_function(states, inputs) * time_step
+
+            probability_to_remain = 1.0
+            for axis, transition_value in zip(state_space.axes, transition_vector):
+                next_element = (
+                    neighbors[axis.name][0]
+                    if transition_value < 0
+                    else neighbors[axis.name][1]
+                )
+
+                probability_to_go_next = abs(transition_value * axis.resolution)
+                probability_to_remain -= probability_to_go_next
+
+                if next_element:
+                    matrix[next_element, element_number] = probability_to_go_next
+
+            if probability_to_remain < 0:
+                raise ValueError("probability_to_remain < 0: time_step is too big")
+
+            matrix[element_number, element_number] = probability_to_remain
+
+    return matrix_set
---
+++ tests/states_space/test_transition_matrix.py
@@ -0,0 +1,94 @@
+import unittest
+
+import numpy as np
+
+from modules.states_space.states_space import StatesSpace
+from modules.states_space.transition_matrix import *
+
+
+# TODO: remove
+from modules.tools.fig_2d import *
+
+
+class TestTransitionMatrix(unittest.TestCase):
+    def test_transition_matrix(self):
+        inputs_set = [
+            (0, 0),
+            (0, 1),
+            (0, -1),
+            (1, 0),
+            (-1, 0),
+        ]
+
+        time_resolution = 0.1
+
+        def dynamic_func(state: "point as list", control_input):
+            return np.array([control_input[0], control_input[1]])
+
+        # create state space
+        statesSpace = StatesSpace()
+
+        statesSpace.add_axis("x", -3, 1, 1)
+        statesSpace.add_axis("y", -1, 9, 0.5)
+
+        statesSpace.create()
+
+        # create transition matrix
+        transition_matrix_set = get_transition_matrix(
+            statesSpace, dynamic_func, inputs_set, time_resolution
+        )
+        # [show_data(transition_matrix.toarray()) for transition_matrix in transition_matrix_set]
+        # [show_data((transition_matrix - transition_matrix_set[0]).toarray()) for transition_matrix in transition_matrix_set]
+
+        self.assertEqual(statesSpace.element_count, 5 * 7)
+
+        for inputs, transition_matrix in zip(inputs_set, transition_matrix_set):
+            if inputs == (0, 0):
+                expect = np.zeros(
+                    (statesSpace.element_count, statesSpace.element_count)
+                )
+                for element_number in range(statesSpace.element_count):
+                    expect[element_number, element_number] = 1.0
+
+                self.assertTrue(np.all(expect == transition_matrix.toarray()))
+
+                # multiple result test.
+                test_x = np.array([i for i in range(statesSpace.element_count)])
+                result = transition_matrix @ test_x
+
+                expect = np.zeros(statesSpace.element_count)
+                for element_number in range(statesSpace.element_count):
+                    expect[element_number] = element_number
+
+                self.assertTrue(np.all(expect == result))
+
+            if inputs == (0, 1):
+                delta = time_resolution * inputs[1] * statesSpace.axes[1].resolution
+                expect = np.zeros(
+                    (statesSpace.element_count, statesSpace.element_count)
+                )
+                for element_number in range(statesSpace.element_count):
+                    expect[element_number, element_number] = 1.0 - delta
+
+                    lower_element_number = element_number - statesSpace.axes[0].length
+                    if lower_element_number >= 0:
+                        expect[element_number, lower_element_number] = delta
+
+                show_data(expect)
+                show_data(transition_matrix.toarray())
+                show_data(transition_matrix.toarray() - expect)
+
+                self.assertTrue(np.all(expect == transition_matrix.toarray()))
+
+                # multiple result test.
+                test_x = np.array([i for i in range(statesSpace.element_count)])
+                result = transition_matrix @ test_x
+
+                expect = np.zeros(statesSpace.element_count)
+                for element_number in range(statesSpace.element_count):
+                    expect[element_number] = element_number * (1.0 - delta)
+                    lower_element_number = element_number - statesSpace.axes[0].length
+                    if lower_element_number > 0:
+                        expect[element_number] += lower_element_number * delta
+
+                self.assertTrue(np.all(expect == result))
