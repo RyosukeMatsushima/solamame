8c4787ac7d2fb71e504d6c797a887fbaef98921d
RyosukeMatsushima <m.ryosuke@me.com>
2023-05-28 17:55:28 +0900

    add fig tool

---
+++ modules/tools/fig_2d.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def show_data(data):
+    plt.figure()
+    plt.imshow(data)
+
+    plt.show()
+
+
+def show_vector_field(data):
+    X, Y = np.meshgrid(range(data.shape[0]), range(data.shape[1]))
+    U = data[:, :, 1]
+    V = data[:, :, 0]
+    fig1, ax1 = plt.subplots()
+    ax1.set_title("Arrows scale with plot width, not view")
+    Q = ax1.quiver(X, Y, U.T, V.T, units="x", pivot="tip", width=0.02, scale=2)
+    print(data)
+    print(X, Y, U, V)
+    plt.show()
