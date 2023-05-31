ac7521b8e3e78d23898a4ff33a9813da257932f5
RyosukeMatsushima <m.ryosuke@me.com>
2023-05-31 21:20:27 +0900

    fix order

--- modules/tools/fig_2d.py
+++ modules/tools/fig_2d.py
@@ -11,8 +11,8 @@ def show_data(data):
 
 def show_vector_field(data):
     X, Y = np.meshgrid(range(data.shape[0]), range(data.shape[1]))
-    U = data[:, :, 1]
-    V = data[:, :, 0]
+    U = data[:, :, 0]
+    V = data[:, :, 1]
     fig1, ax1 = plt.subplots()
     ax1.set_title("Arrows scale with plot width, not view")
     Q = ax1.quiver(X, Y, U.T, V.T, units="x", pivot="tip", width=0.02, scale=2)
