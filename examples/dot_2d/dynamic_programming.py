1af8c7b92f861d896771a5e1c6092f60d7e8bf8c
RyosukeMatsushima <m.ryosuke@me.com>
2023-07-13 09:18:00 +0900

    refactor to use same params

---
+++ examples/dot_2d/dynamic_programming.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+from modules.dynamic_programming.dynamic_programming import DynamicProgramming
+from modules.states_space.transition_matrix import *
+
+from examples.dot_2d.base import *
+
+def stage_cost_map(inputs):
+    return np.array([stage_cost_function(element_number, inputs) for element_number in range(statesSpace.element_count)])
+
+def terminal_cost_map():
+    return np.array([terminal_cost_function(element_number) for element_number in range(statesSpace.element_count)])
+
+def transition_function(element_number, inputs):
+    states = list(statesSpace.get_states_from(element_number).values())
+    return dynamics(states, inputs)
+
+time_resolution = 0.1
+
+# create transition matrix
+transition_matrix_set = get_transition_matrix(
+    statesSpace, transition_function, inputs_set, time_resolution
+)
+
+dynamicProgramming = DynamicProgramming(
+    statesSpace,
+    transition_matrix_set,
+    stage_cost_map,
+    terminal_cost_map,
+    inputs_set,
+    0.00001,
+    100000,
+    600
+)
+
+
+def debug_func():
+    print("debug_func")
+    sheet = dynamicProgramming.current_value_function.get_2d_sheet(
+        "x", "y", {"x": 0, "y": 0}
+    )
+    sheet = np.array(sheet)
+    sheet = np.where(sheet > 60, 60, sheet)
+    show_data(sheet)
+
+    input_index = dynamicProgramming.inputs_space.get_2d_sheet(
+        "x", "y", {"x": 0, "y": 0}
+    )
+    print(input_index)
+    show_data(input_index)
+
+    input_vector_space = [[inputs_set[index] for index in l] for l in input_index]
+    print(input_vector_space)
+
+    show_vector_field(np.array(input_vector_space))
+
+
+dynamicProgramming.calculate(debug_func)
+
+evaluate(dynamicProgramming.inputs_space, dynamicProgramming.current_value_function)
---
+++ examples/dot_2d/proposed_method.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+from modules.proposed_method.find_cost_to_go import FindCostToGo
+from modules.states_space.transition_matrix import *
+
+from examples.dot_2d.base import *
+
+time_resolution = 0.01
+min_cost = 0
+max_cost = 130
+
+proballistic_space = statesSpace
+
+def transition_function(element_number, inputs):
+    states = list(proballistic_space.get_states_from(element_number).values())
+    stage_cost = stage_cost_function(element_number, inputs)
+    states_dt = dynamics(states, inputs) 
+
+    if stage_cost <= 0:
+        if not np.all(states_dt == 0.0):
+            raise RuntimeError("invalid cost value. it's devided by zero")
+
+        return np.zeros(len(states_dt))
+
+    return states_dt / stage_cost
+
+# create transition matrix
+print("calculate transition_matrix_set")
+transition_matrix_set = get_transition_matrix(
+    proballistic_space, transition_function, inputs_set, time_resolution
+)
+
+goal_element_number = proballistic_space.get_element_number({"x": goal[0], "y": goal[1]})
+proballistic_space.values[goal_element_number] = 1
+
+findCostToGo = FindCostToGo(
+    proballistic_space,
+    transition_matrix_set,
+    time_resolution,
+    max_cost,
+    min_cost
+)
+
+
+print("start findCostToGo")
+findCostToGo.calculate()
+
+evaluate(findCostToGo.inputs_space, findCostToGo.cost_to_go_space)
