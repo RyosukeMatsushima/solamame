c631dc7912c6023812dfd290a28c96d47a12179e
RyosukeMatsushima <m.ryosuke@me.com>
2023-09-02 23:29:27 +0900

    update dot_2d condition

--- examples/dot_2d/base.py
+++ examples/dot_2d/base.py
@@ -22,7 +22,7 @@ class Base(BaseCommon):
         ]
 
         # Define the start and goal points
-        self.goal_state = {"x": 1, "y": 2}
+        self.goal_state = {"x": 1, "y": 1}
         self.goal = (self.goal_state["x"], self.goal_state["y"])
 
         # create state space
@@ -31,32 +31,36 @@ class Base(BaseCommon):
         self.statesSpace.add_axis("y", 0, 8, 10)
         self.statesSpace.create()
 
-        self.init_state = {"x": 3, "y": 7}
+        self.init_state = {"x": 9, "y": 7}
         self.model = Dot2D((self.init_state["x"], self.init_state["y"]))
         self.dynamics = self.model.dynamics
 
-    def is_obstacle(self, element_number):
+    def is_obstacle_0(self, element_number):
         state = self.statesSpace.get_states_from(element_number)
 
-        if 1 < state["x"] < 3 and 5 < state["y"] < 6:
-            return True
-        if 4 < state["x"] < 7 and 5 < state["y"] < 6:
-            return True
-        if 2 < state["x"] < 5 and 3 < state["y"] < 4:
-            return True
+        if 2 < state["x"] < 7 and 3 < state["y"] < 6:
+            return 1.0
 
-        return False
+        return 0.0
+
+    def is_obstacle_1(self, element_number):
+        state = self.statesSpace.get_states_from(element_number)
+
+        if 3 < state["x"] < 8 and 2 < state["y"] < 5:
+            return 1.0
+
+        return 0.0 
 
     def stage_cost_function(self, element_number, inputs):
         goal_cost = 0.0 if self.is_goal(element_number) else 1.0
-        obstacle_cost = 1.0 if self.is_obstacle(element_number) else 0.0
+        obstacle_cost = self.is_obstacle_0(element_number) + self.is_obstacle_1(element_number)
         inputs_norm = sum([i**2 for i in inputs]) ** 0.5
         edge_cost = 1.0 if self.is_edge(element_number) else 0.0
-        return inputs_norm * 1 + goal_cost * 1 + obstacle_cost * 100 + edge_cost * 1000
+        return inputs_norm * 0.5 + goal_cost * 1 + obstacle_cost * 10 + edge_cost * 1000
 
     def evaluate(self, inputs_space, cost_to_go_space):
         start_time = 0.0
-        end_time = 10.0
+        end_time = 20.0
 
         result = simulate(
             self.model, inputs_space, self.inputs_set, start_time, end_time
